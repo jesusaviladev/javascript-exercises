bbb895a7f77a3591de0b091056e502d49ecf3c20
Jesus Avila <avijesus13@gmail.com>
2024-07-10 10:48:57 -0400

    seventh exercise solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  if (typeof temperature !== "number") return "ERROR";
+  const celsius = (temperature - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10; // rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  if (typeof temperature !== "number") return "ERROR";
+  const fahrenheit = (temperature * 9) / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10; // rounded
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
