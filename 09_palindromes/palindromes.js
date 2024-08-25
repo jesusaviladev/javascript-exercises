c8ffc4b90b0d097e00569e8206f8eb537e7a6ae0
Jesus Avila <avijesus13@gmail.com>
2024-08-25 10:32:25 -0400

    9th exercise solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const sanitizedString = string.toLowerCase().replace(/[^a-z0-9]/gi, "");
 
+  const palindrome = sanitizedString.split("").reverse().join("");
+  return palindrome === sanitizedString;
 };
 
 // Do not edit below this line
