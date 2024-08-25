bd8199e7261000f379ec3aee926852cd3a7920f0
Jesus Avila <avijesus13@gmail.com>
2024-08-25 10:57:19 -0400

    exercises 10 and 11 solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const finalNum = Number(num);
 
+  if (finalNum === 0) return 0;
+
+  if (isNaN(finalNum) || finalNum < 0) return "OOPS";
+
+  const fibonaccis = [1, 1];
+  const end = finalNum - fibonaccis.length;
+
+  for (let i = 1; i <= end; i++) {
+    fibonaccis.push(
+      fibonaccis[fibonaccis.length - 2] + fibonaccis[fibonaccis.length - 1]
+    );
+  }
+
+  return fibonaccis[finalNum - 1];
 };
 
 // Do not edit below this line
